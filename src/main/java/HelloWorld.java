20f69444f43cc242139dcf4ac7889d298019529e
iTanveerAhmad <talktanveerkhan@outlook.com>
2021-05-24 11:09:05 +0200

    Update project with using StringUtils dep into project.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
 
 public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println(StringUtils.upperCase("hello world"));
     }
 
 }
